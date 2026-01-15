dc23a1516ff7af3233da12650c0718c26accc81e
Ayushrai0 <144903405+Ayushrai0@users.noreply.github.com>
2026-01-15 16:28:12 +0530

    added more controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,15 @@
-import {Router} from "express";
+import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 
-const router=Router()
-router.route("/register").post(registerUser)
+const router = Router();
 
-export default router
+router.route("/register").post(
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
+
+export default router;
